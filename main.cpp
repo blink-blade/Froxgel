bcb9422fe682b3b21484cffb9f9fa91b147879c6
blink-blade <micah@blinkblade.net>
2025-12-24 20:57:02 -0500

    Triangles test.

--- main.cpp
+++ main.cpp
@@ -13,10 +13,36 @@ using namespace std;
 int main() {
     engineInits();
     vector<float> vertices {
-        -500.5f, -500.5f, 0.0f, 0.0f, 0.0f,
-         500.5f, -500.5f, 500.0f, 0.5f, 1.0f,
-         500.0f,  500.5f, 0.0f, 1.0f, 0.0f
+        // -500.5f, -500.5f, 0.0f, 0.0f, 0.0f,
+        //  500.5f, -500.5f, 500.0f, 0.5f, 1.0f,
+        //  500.0f,  500.5f, 0.0f, 1.0f, 0.0f
     };
+    for (int x = 0; x < 6000; x += 10) {
+        for (int y = 0; y < 6000; y += 10) {
+            vertices.push_back(x);
+            vertices.push_back(y);
+            vertices.push_back(0.0f);
+
+            vertices.push_back(0.0f);
+            vertices.push_back(0.0f);
+
+
+            vertices.push_back(x + 5);
+            vertices.push_back(y);
+            vertices.push_back((x + y) / 10);
+
+            vertices.push_back(0.5f);
+            vertices.push_back(1.0f);
+
+
+            vertices.push_back(x + 10);
+            vertices.push_back(y);
+            vertices.push_back(0.0f);
+
+            vertices.push_back(1.0f);
+            vertices.push_back(0.0f);
+        }
+    }
     Mesh mesh("vec3 vec2", vertices, "base_tex", "base_tex");
 
     while (!window.shouldClose()) {
