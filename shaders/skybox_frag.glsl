9fe89c3d299b001eb272e84a2dc5cb532f1906a6
blink-blade <micah@blinkblade.net>
2026-01-06 21:05:16 -0500

    Okay, this one is pretty cool

--- shaders/skybox_frag.glsl
+++ shaders/skybox_frag.glsl
@@ -9,16 +9,17 @@ void main()
     vec3 toCam = normalize(FragPos - cameraPos);
     float smallTime = time / 10;
     float smallerTime = time / 25;
-    float noise = layeredNoise3D(toCam.x, toCam.y, toCam.z, 5, 2);
-    float steps = 25.0;
+    float noise = layeredNoise3D(toCam.x, toCam.y, toCam.z, 5, 1);
+    float steps = 250.0;
     float val = floor(noise * steps) / steps;
 
-    vec3 warpedDir = (toCam + layeredNoise3D(toCam.x * 3, toCam.y * 3, toCam.z * 3, 5, 2) * 0.3) * 4;
-    float bands = sin(warpedDir.y * 10.0 + layeredNoise3D(warpedDir.x, warpedDir.y, warpedDir.z, 5, 2));
-    vec3 color = mix(
-        vec3(0.2, 0.1, 0.4),
-        vec3(0.8, 0.3, 1.0),
-        bands * 0.5 + 0.5
+    float corruption = layeredNoise3D(toCam.x * 3.0, toCam.y * 3.0, toCam.z * 3.0, 5, 1);
+    vec3 sickSky = mix(
+        vec3(0.2, 0.4, 0.1),
+        vec3(0.8, 0.1, 0.1),
+        corruption
     );
+    sickSky *= 1.0 + sin(time + corruption * 10.0) * 0.5;
+    vec3 color = vec3(val, val, val) * sickSky;
     FragColor = vec4(color, 1.0);
 }
