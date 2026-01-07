8aa48cbafd7c4d36141d1f8665108542d7051737
blink-blade <micah@blinkblade.net>
2026-01-06 20:55:40 -0500

    hsv cycling. TRIPPY

--- shaders/common.glsl
+++ shaders/common.glsl
@@ -190,6 +190,12 @@ float layeredNoise3D(float x, float y, float z, int layerAmount, float frequency
     return val;
 }
 
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0/3.0, 1.0/3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
 
 vec2 clipSpaceToTexCoords(vec4 clipSpace) {
     vec2 ndc = (clipSpace.xy / clipSpace.w);
--- shaders/skybox_frag.glsl
+++ shaders/skybox_frag.glsl
@@ -9,17 +9,12 @@ void main()
     vec3 toCam = normalize(FragPos - cameraPos);
     float smallTime = time / 10;
     float smallerTime = time / 25;
-    float noise = layeredNoise3D(toCam.x, toCam.y, toCam.z, 12, 2);
+    float noise = layeredNoise3D(toCam.x, toCam.y, toCam.z, 1, 2);
     float steps = 25.0;
     float val = floor(noise * steps) / steps;
 
-    float h = toCam.y * 0.5 + 0.5;
+    float hue = fract(val * 0.3 + time * 0.1);
+    vec3 color = hsv2rgb(vec3(hue, 0.7, 1.0)) * 0.5;
 
-    vec3 horizon = vec3(0.0, 0.6, 0.3);
-    vec3 zenith  = vec3(0.1, 0.2, 0.6);
-
-    vec3 baseSky = mix(horizon, zenith, smoothstep(0.0, 1.0, h));
-    vec3 finalColor = baseSky + val * 0.1;
-
-    FragColor = vec4(finalColor, 1.0);
+    FragColor = vec4(color, 1.0);
 }
