d9b008c13a79014786ac3e585b67d7727292958e
blink-blade <micah@blinkblade.net>
2026-01-06 21:09:46 -0500

    Okay, soft on the eyes for a bit. I'll mess around with formulas on the noise itself with this for colors.

--- main.cpp
+++ main.cpp
@@ -32,6 +32,7 @@ int main() {
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
         // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         engineUpdates();
+        cout << timeValue << endl;
 
         glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
--- shaders/skybox_frag.glsl
+++ shaders/skybox_frag.glsl
@@ -10,16 +10,11 @@ void main()
     float smallTime = time / 10;
     float smallerTime = time / 25;
     float noise = layeredNoise3D(toCam.x, toCam.y, toCam.z, 5, 1);
-    float steps = 250.0;
+    float steps = 255.0;
     float val = floor(noise * steps) / steps;
 
-    float corruption = layeredNoise3D(toCam.x * 3.0, toCam.y * 3.0, toCam.z * 3.0, 5, 1);
-    vec3 sickSky = mix(
-        vec3(0.2, 0.4, 0.1),
-        vec3(0.8, 0.1, 0.1),
-        corruption
-    );
-    sickSky *= 1.0 + sin(time + corruption * 10.0) * 0.5;
-    vec3 color = vec3(val, val, val) * sickSky;
+    float hue = fract(val * 0.3 + (time + 65) * 0.01);
+    vec3 color = hsv2rgb(vec3(hue, 0.7, 1.0)) * 1;
+
     FragColor = vec4(color, 1.0);
 }
