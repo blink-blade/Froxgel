e4ec5de117f062d1d49319c1ca038bffcef44ad7
blink-blade <micah@blinkblade.net>
2026-01-10 13:58:31 -0500

    Nebula kinda stuff

--- shaders/skybox_frag.glsl
+++ shaders/skybox_frag.glsl
@@ -9,11 +9,11 @@ void main()
     vec3 toCam = normalize(FragPos - cameraPos);
     float smallTime = time / 10;
     float smallerTime = time / 25;
-    float noise = layeredNoise3D(toCam.x, toCam.y, toCam.z, 15, 5);
+//    float noise = layeredNoise3D(toCam.x, toCam.y, toCam.z, 15, 5);
     vec3 flow = vec3(
-    layeredNoise3D(toCam + vec3(0.1, 0.0, 0.0), 15, 2),
-    layeredNoise3D(toCam + vec3(0.0, 0.1, 0.0), 15, 2),
-    layeredNoise3D(toCam + vec3(0.0, 0.0, 0.1), 15, 2)
+    layeredNoise3D(toCam + vec3(0.1, 0.0, 0.0), 4, 2),
+    layeredNoise3D(toCam + vec3(0.0, 0.1, 0.0), 3, 2),
+    layeredNoise3D(toCam + vec3(0.0, 0.0, 0.1), 2, 2)
     );
 
 //    vec3 color = vec3(abs(flow), flow, abs(flow));
