1d4223f3b3e75717c48f83eb5081cdfb1a413b6e
blink-blade <micah@blinkblade.net>
2026-01-10 14:58:57 -0500

    Another one I'll use.

--- shaders/skybox_frag.glsl
+++ shaders/skybox_frag.glsl
@@ -7,7 +7,7 @@ uniform sampler2D skyboxTexture;
 vec3 toCam;
 
 vec3 getCracksColor() {
-    float noise = layeredNoise3D(toCam.x, toCam.y, toCam.z, 15, 5);
+    float noise = layeredNoise3D(toCam.x, toCam.y, toCam.z, 3, 2);
     float cracks = abs(fract(noise * 4.0) - 0.5);
 
     return vec3(abs(cracks), cracks, abs(cracks));
@@ -26,24 +26,20 @@ void main()
     layeredNoise3D(toCam + vec3(0.0, 0.0, 0.1) * smallTime, 2, 2)
     );
 
-    // Make terraced noise for the sky gradient, because why not?
     float noise = layeredNoise3D(toCam, 12, 2);
-    float steps = 7.0;
-    float val = floor(noise * steps) / steps;
 
     // Do a fancy formula to make the sky change color the higher it is.
     float h = toCam.y * 0.5 + 0.5;
     vec3 horizon = vec3(0.0, 0.0, 0.25);
     vec3 zenith  = vec3(0.1, 0.2, 0.6);
 
-    vec3 baseSky = mix(horizon, zenith, smoothstep(0.0, 1.0, h));
-    vec3 gradient = baseSky + val * 0.1;
+    vec3 gradient = mix(horizon, zenith, smoothstep(0.0, 1.0, h));
 
     // Mix the gradient with the nebula.
     vec3 gradientWithNebula = mix(gradient, nebula, 1.3 * h);
 
     // Make some noise to add some more texture to it.
-    vec3 additiveNoiseColor = vec3(layeredNoise3D(toCam.x, toCam.y, toCam.z, 2, 5) + vec3(0.1, 0.1, 0.0) * sin(time / 2));
+    vec3 additiveNoiseColor = vec3(layeredNoise3D(toCam.x, toCam.y, toCam.z, 2, 5) + vec3(0.1, 0.1, 0.0) * sin(time * 0.5));
     additiveNoiseColor *= vec3(0.1, 0.25, 0.15) + (smallTime - 0.5) * 0.5;
 
     // Add the noise to the final color.
