a94f4e441700ae412d5bc675386b7d9f80d717af
blink-blade <micah@blinkblade.net>
2026-01-25 13:21:08 -0500

    Smoother.

--- shaders/simple_lighting_frag.glsl
+++ shaders/simple_lighting_frag.glsl
@@ -20,9 +20,24 @@ float ShadowCalculation(vec4 fragPosLightSpace, DirLight light, vec3 normal, vec
     float currentDepth = projCoords.z;
     // check whether current frag pos is in shadow
     float bias = max(0.05 * (1.0 - dot(normal, -light.direction)), 0.004);
-    float shadow = currentDepth - bias > closestDepth  ? 1.0 : 0.0;
+    // check whether current frag pos is in shadow
+    // float shadow = currentDepth - bias > closestDepth  ? 1.0 : 0.0;
+    // PCF
+    float shadow = 0.0;
+    vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
+    for(int x = -1; x <= 1; ++x)
+    {
+        for(int y = -1; y <= 1; ++y)
+        {
+            float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r;
+            shadow += currentDepth - bias > pcfDepth  ? 1.0 : 0.0;
+        }
+    }
+    shadow /= 9.0;
+
+    // keep the shadow at 0.0 when outside the far_plane region of the light's frustum.
     if(projCoords.z > 1.0)
-        shadow = 0.0;
+    shadow = 0.0;
     return shadow;
 }
 
