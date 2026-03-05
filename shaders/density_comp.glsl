0fe04b67fe371b28ee2f02a6094e88b27636e9d5
blink-blade <micah@blinkblade.net>
2026-03-04 21:21:48 -0500

    Fractal visualization on the skybox

--- shaders/density_comp.glsl
+++ shaders/density_comp.glsl
@@ -13,7 +13,34 @@ uint index3D(uint x, uint y, uint z)
     return x + y * gridSizeX + z * gridSizeX * gridSizeY;
 }
 
-float surfaceLevel = 30;
+// Compute Zn^2 + C.
+vec2 computeNext(vec2 current, vec2 constant) {
+    // Zn^2
+    const float zr = current.x * current.x - current.y * current.y;
+    const float zi = 2.0 * current.x * current.y;
+
+    return vec2(zr, zi) + constant;
+}
+
+float mod2(vec2 z) {
+    return dot(z, z);  // x*x + y*y
+}
+
+// Computes sequence elements until mod exceeds threshold or max iteration is reached.
+float computeIterationsSmooth(vec2 z0, vec2 constant, int maxIterations) {
+    vec2 zn = z0;
+    int iteration = 0;
+    while (mod2(zn) < 4.0 && iteration < maxIterations) {
+        zn = computeNext(zn, constant);
+        iteration++;
+    }
+
+    const float modi = sqrt(mod2(zn));
+    const float smooth_iteration = float(iteration) - log2(max(1.0f, log2(modi)));
+    return smooth_iteration;
+}
+
+float surfaceLevel = 0.0;
 void main()
 {
     uvec3 id = gl_GlobalInvocationID;
@@ -24,7 +51,8 @@ void main()
     uint index = index3D(id.x, id.y, id.z);
 //    density[index] = layeredNoise3D(id.x + time * 4, id.y, id.z, 2, 0.01 + sin(time) / 100);
 //    density[index] = (sin(id.x / 10) + sin((id.y + time) / 10) + sin(id.z / 10));
-    float noise = layeredNoise3D(id.x + time * 200, id.y, id.z, 2, 0.01);
-    noise -= (surfaceLevel - id.y) / 50;
-    density[index] = noise;
+//    float noise = layeredNoise3D(id.x + time * 200, id.y, id.z, 2, 0.01);
+//    noise -= (surfaceLevel - id.y) / 50;
+    float iterations = computeIterationsSmooth(id.xy / 2, id.xy / 2, 50);
+    density[index] = iterations / 50;
 }
--- shaders/skybox_frag.glsl
+++ shaders/skybox_frag.glsl
@@ -44,31 +44,60 @@ vec3 getSunAddition() {
     return (sunRGB * 3 * max(0.0f, 0.3f - distance)) * 5;
 }
 
+// Compute Zn^2 + C.
+vec2 computeNext(vec2 current, vec2 constant) {
+    // Zn^2
+    const float zr = current.x * current.x - current.y * current.y;
+    const float zi = 2.0 * current.x * current.y;
+
+    return vec2(zr, zi) + constant;
+}
+
+float mod2(vec2 z) {
+    return dot(z, z);  // x*x + y*y
+}
+
+// Computes sequence elements until mod exceeds threshold or max iteration is reached.
+float computeIterationsSmooth(vec2 z0, vec2 constant, int maxIterations) {
+    vec2 zn = z0;
+    int iteration = 0;
+    while (mod2(zn) < 4.0 && iteration < maxIterations) {
+        zn = computeNext(zn, constant);
+        iteration++;
+    }
+
+    const float modi = sqrt(mod2(zn));
+    const float smooth_iteration = float(iteration) - log2(max(1.0f, log2(modi)));
+    return smooth_iteration;
+}
+
 void main()
 {
     toCam = normalize(FragPos - cameraPos);
     float sinTime = sin(time * 0.07);
 
     // Get the main nebula color.
-    float noise = layeredNoise3D(toCam + vec3(0.0, 0.1, 0.0) * time / 10, 3, 2);
-    vec3 nebula = vec3(
-    layeredNoise3D(toCam + vec3(0.1, 0.0, 0.0) * sinTime, 4, 2),
-    noise,
-    layeredNoise3D(toCam + vec3(0.0, 0.0, 0.1) * sinTime, 2, 2)
-    );
-
-    // Do a fancy formula to make the sky change color the higher it is.
-    float h = toCam.y * 0.5 + 0.5;
-    vec3 gradient = getGradientColor(h);
-
-    // Mix the gradient with the nebula.
-    vec3 gradientWithNebula = mix(gradient, nebula, 1.3 * h);
-
-    // Get the ridge color.
-    vec3 cracks = getCracksColor(noise) * vec3(0.2, 0.25, 0.3);
-
-    vec3 finalColor = gradientWithNebula * 2 + cracks;
-    finalColor += getSunAddition();
+//    float noise = layeredNoise3D(toCam + vec3(0.0, 0.1, 0.0) * time / 10, 3, 2);
+//    vec3 nebula = vec3(
+//    layeredNoise3D(toCam + vec3(0.1, 0.0, 0.0) * sinTime, 4, 2),
+//    noise,
+//    layeredNoise3D(toCam + vec3(0.0, 0.0, 0.1) * sinTime, 2, 2)
+//    );
+//
+//    // Do a fancy formula to make the sky change color the higher it is.
+//    float h = toCam.y * 0.5 + 0.5;
+//    vec3 gradient = getGradientColor(h);
+//
+//    // Mix the gradient with the nebula.
+//    vec3 gradientWithNebula = mix(gradient, nebula, 1.3 * h);
+//
+//    // Get the ridge color.
+//    vec3 cracks = getCracksColor(noise) * vec3(0.2, 0.25, 0.3);
+//
+//    vec3 finalColor = gradientWithNebula * 2 + cracks;
+//    finalColor += getSunAddition();
+    float iterations = computeIterationsSmooth(toCam.xy, toCam.xy, 500);
+    vec3 finalColor = vec3(iterations / 500);
 
     FragColor = vec4(finalColor, 1.0);
 }
