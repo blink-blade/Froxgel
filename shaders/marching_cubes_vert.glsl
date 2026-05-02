63c99d01f17cf139ca5be5bd3e4ef7117d1b3f88
blink-blade <micah@blinkblade.net>
2026-05-02 17:43:23 -0400

    About to add collision

--- shaders/marching_cubes_vert.glsl
+++ shaders/marching_cubes_vert.glsl
@@ -1,17 +1,19 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
-layout (location = 2) in float aIndex;
+layout (location = 2) in vec3 aColor;
+out vec3 Color;
+out vec3 Normal;
+out vec3 FragPos;
 
 out VS_OUT {
     vec3 FragPos;
-    float Index;
     vec3 Normal;
     vec4 FragPosLightSpace;
 } vs_out;
 
 void main()
 {
-    vs_out.Index = aIndex;
+    Color = aColor;
     vs_out.FragPos = vec3(aPos.x, aPos.y, aPos.z) + offset;
     vs_out.Normal = aNormal;
     vs_out.FragPosLightSpace = sunSpaceMatrix * vec4(vs_out.FragPos, 1.0);
